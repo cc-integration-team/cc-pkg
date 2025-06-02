89113492025bc44f9ee24d9ff5af154aeb570005
dangquyitt <quy.nd@namitech.io>
2025-06-02 15:24:46 +0700

    bugfix redis impl subsciber

--- pkg/pubsub/subscriber.go
+++ pkg/pubsub/subscriber.go
@@ -3,8 +3,7 @@ package pubsub
 import "context"
 
 type HandlerFunc func(ctx context.Context, message []byte)
-type CloseFunc func() error
 
 type Subscriber interface {
-	Subscribe(ctx context.Context, topic string, handler HandlerFunc) (CloseFunc, error)
+	Subscribe(ctx context.Context, topic string, handler HandlerFunc) error
 }
